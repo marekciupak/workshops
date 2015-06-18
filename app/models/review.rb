53597b4210c856d238beb2c0815f313cf4320487
Marek Ciupak <ciupakm@gmail.com>
2015-06-19 00:58:31 +0200

    added validation for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :rating, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
