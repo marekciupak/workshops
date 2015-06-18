c885dbf3179fafe89a9204351c3128de81544b42
Marek Ciupak <ciupakm@gmail.com>
2015-06-19 00:05:58 +0200

    improved user table

--- db/migrate/20150615115949_add_details_to_users.rb
+++ db/migrate/20150615115949_add_details_to_users.rb
@@ -2,6 +2,6 @@ class AddDetailsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :firstname, :string
     add_column :users, :lastname, :string
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, :default => false
   end
 end
