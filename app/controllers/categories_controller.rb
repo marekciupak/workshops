319084045ff8f14bae4d47da3561a41a37eb834a
Marek Ciupak <ciupakm@gmail.com>
2015-06-19 00:01:17 +0200

    added checking if user is an admin in categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
+  before_action :check_if_user_is_admin, only: [:new, :edit, :update, :destroy, :create]
   
   expose(:categories)
   expose(:category)
@@ -41,7 +42,13 @@ class CategoriesController < ApplicationController
   end
 
   private
-    def category_params
-      params.require(:category).permit(:name)
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def check_if_user_is_admin
+    unless current_user.admin?
+      redirect_to new_user_session_path
     end
+  end
 end
