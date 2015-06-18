13a3079ebcb4daa9fe3e6c7a5848b978205eec2e
Marek Ciupak <ciupakm@gmail.com>
2015-06-18 18:07:22 +0200

    added additional fields to the user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :firstname << :lastname
+    devise_parameter_sanitizer.for(:account_update) << :firstname << :lastname
+  end
+
 end
