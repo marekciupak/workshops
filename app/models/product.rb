c8402bdc32f2887fe8f2c6802fc112f19c61cf35
Marek Ciupak <ciupakm@gmail.com>
2015-06-19 00:40:52 +0200

    added validation and calculate average rating for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ }
+
+  def average_rating
+    self.reviews.average("rating")
+  end
 end
